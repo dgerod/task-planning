2614c240eafdc90d30dfdfcd4a8582ec1892e9d1
Diego Escudero <dgerod@xyz-lab.org.es>
2017-11-01 12:19:58 +0100

    Re-organize planners and tests

--- planners/tests/Contingent-FF/run_c-ff_test.sh
+++ planners/tests/Contingent-FF/run_c-ff_test.sh
@@ -2,7 +2,7 @@
 # Executes a test as a domain and problem pddl files.
 # Usage: ./run_ff_tests.sh 'test_name'
 
-timeout 10 ../../planners/Contingent-FF -o $1_domain.pddl -f $1_problem.pddl &> $1_plan.txt
+timeout 10 ../../bin/Contingent-FF -o $1_domain.pddl -f $1_problem.pddl &> $1_plan.txt
 
 echo "--------------------------"
 echo "Returned plan"
--- planners/tests/ff/run_ff_test.sh
+++ planners/tests/ff/run_ff_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Executes a test as a domain and problem pddl files.
-# Usage: ./run_popf_tests.sh 'test_name'
+# Usage: ./run_ff_tests.sh 'test_name'
 
-timeout 10 ../../planners/popf -n $1_domain.pddl $1_problem.pddl &> $1_plan.txt
+timeout 10 ../../bin/ff -o $1_domain.pddl -f $1_problem.pddl &> $1_plan.txt
 
 echo "--------------------------"
 echo "Returned plan"
